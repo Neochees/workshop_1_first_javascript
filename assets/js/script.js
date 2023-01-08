7fb1d6fced746187bb1201d00738f92faba12289
Neochees <113420296+Neochees@users.noreply.github.com>
2023-01-08 16:44:58 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+var eersteVariabele = "test";
+console.log(eersteVariabele);
+
+var voornaam = "Thijmen"; // wijzig dit in jouw eigen voornaam
+var tussenvoegsel = ""; // wijzig dit in jouw eigen eventuele tussenvoegsel
+var achternaam = "Lindhout"; // wijzig dit in jouw eigen achternaam
+var volledigeNaam = "Thijmen Lindhout"; // plak hier de 3 bovenstaande variabelen aan elkaar, met spaties
+var geboortedag = "23"; // je geboortedag
+var geboortemaand = "Maart"; // je geboortemaand
+var geboortejaar = "2006"; // je geboortejaar
+var huidig_jaartal = "2022"// het huidige jaar;
+var leeftijd = 16; // bereken hier jouw leeftijd op basis van de eerder ingevulde geboortegegevens.
+var dag_van_het_jaar = 6; // bereken hier hoeveelste dag van dit kalenderjaar het is
+var straat = "rozenstraat"; // je straat;
+var huisnummer = "2"; // je huisnummer;
+var woonplaats = "Alphen aan den Rijn"; // je woonplaats;
+var woonachtig_in_utrecht = "False"; // check hier dmv een boolean of jouw woonplaats gelijk is aan "Utrecht"
+var aantal_studenten_in_deze_klas = 21;
+var kosten_van_frikandelbroodje = 1.76;
+var totale_lunchkosten = 73.92; // bereken hier het totaalbedrag als iedere student 2 frikandelbroodjes zou kopen
+
+document.write("Voornaam: " + voornaam);
+document.write("<br>");
+document.write("Achternaam: " + achternaam);
+document.write("<br>");
+document.write("Volledigenaam: " + volledigeNaam);
+document.write("<br>");
+document.write("Geboortedag: " + geboortedag)
+document.write("<br>");
+document.write("Geboortemaand: " + geboortemaand)
+document.write("<br>");
+document.write("Geboortejaar: " + geboortejaar)
+document.write("<br>");
+document.write("Huidig jaar: " + huidig_jaartal)
+document.write("<br>");
+document.write("Leeftijd: " + leeftijd)
+document.write("<br>");
+document.write("Dag van het jaar: " + dag_van_het_jaar)
+document.write("<br>");
+document.write("Straat: " + straat)
+document.write("<br>");
+document.write("Huisnummer: " + huisnummer)
+document.write("<br>");
+document.write("Woonplaats: " + woonplaats)
+document.write("<br>");
+document.write("Woonachtig in Utrecht: " + woonachtig_in_utrecht)
+document.write("<br>");
+document.write("Aantal studenten in deze klas: " + aantal_studenten_in_deze_klas)
+document.write("<br>");
+document.write("Kosten van frikandelbroodje: " + kosten_van_frikandelbroodje)
+document.write("<br>");
+document.write("Totale lunchkosten: " + totale_lunchkosten)
+/* toon de waardes hieronder op het scherm gescheiden door het html: '<br>' element voor een nieuwe-regel
+  bijvoorbeeld:
+  document.write("Voornaam: " + voornaam);
+  document.write("<br>");
+  document.write("Achternaam: " + achternaam);
+  enzovoorts, zorg dat alle bovenstaande variabelen op het scherm komen
+*/
